2faacf220a1d4c90160197275e660d1f4a54ddac
Mark Johnson <mark.johnson3@centene.com>
2021-06-21 12:10:01 -0500

    Go setup changes (old was deprecated)

---
+++ doom/.doom.d/config.el
@@ -0,0 +1,153 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Mark Johnson"
+      user-mail-address "mark@jhns.in")
+
+;; Emacs is transparent?
+(set-frame-parameter (selected-frame) 'alpha '(100 100))
+(add-to-list 'default-frame-alist '(alpha 100 100))
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font "Iosevka Term" :size 13))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-nova)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(setq evil-normal-state-cursor '(box "lime green")
+      evil-insert-state-cursor '(bar "firebrick")
+      evil-visual-state-cursor '(hollow "orange"))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; (setq display-line-numbers-type t)
+;; (setq-default display-line-numbers-mode t)
+;; (setq display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+
+;; Disable the horrible quitting messages
+(setq confirm-kill-emacs nil)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :init
+)
+
+;; (setq lsp-ui-doc-enable nil
+;;       lsp-ui-peek-enable t
+;;       lsp-ui-sideline-enable t
+;;       lsp-ui-imenu-enable t
+;;       lsp-ui-flycheck-enable t)
+
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands (lsp lsp--buffer-deferred))
+
+;; (defun lsp-go-install-save-hooks ()
+;;   (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;;   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;;Optional - provides fancier overlays.
+
+
+;; Go stuff
+;; (add-hook 'go-mode-hook 'lsp-deferred)
+;; (setq gofmt-command "goimports")
+;; (setq lsp-gopls-staticcheck t)
+;; (setq lsp-gopls-complete-unimported t)
+
+;; (setq lsp-eldoc-render-all t)
+
+;; (add-hook 'before-save-hook 'gofmt-before-save)
+;; (add-hook 'after-init-hook 'global-company-mode)
+
+;; (require 'go-complete)
+;; (add-hook 'completion-at-point-functions 'go-complete-at-point)
+
+;; (add-hook 'go-mode-hook (lambda ()
+;;                         (set (make-local-variable 'company-backends) '(company-go))
+;;                         (company-mode)))
+
+;; enable flycheck
+;; (add-hook 'go-mode-hook 'flycheck-mode)
+
+;; don't even need it since there's already quite a lot built in !!!
+;; (add-to-list 'yas-snippet-dirs "~/gits/yasnippet-go")
+
+;; Bonus: escape analysis.
+;; (require 'flycheck)
+;; (flycheck-define-checker go-build-escape
+;;   "A Go escape checker using `go build -gcflags -m'."
+;;   :command ("go" "build" "-gcflags" "-m"
+;;             (option-flag "-i" flycheck-go-build-install-deps)
+;;             ;; multiple tags are listed as "dev debug ..."
+;;             (option-list "-tags=" flycheck-go-build-tags concat)
+;;             "-o" null-device)
+;;   :error-patterns
+;;   (
+;;    (warning line-start (file-name) ":" line ":"
+;;           (optional column ":") " "
+;;           (message (one-or-more not-newline) "escapes to heap")
+;;           line-end)
+;;    (warning line-start (file-name) ":" line ":"
+;;           (optional column ":") " "
+;;           (message "moved to heap:" (one-or-more not-newline))
+;;           line-end)
+;;    (info line-start (file-name) ":" line ":"
+;;           (optional column ":") " "
+;;           (message "inlining call to " (one-or-more not-newline))
+;;           line-end)
+;;   )
+;;   :modes go-mode
+;;   :predicate (lambda ()
+;;                (and (flycheck-buffer-saved-p)
+;;                     (not (string-suffix-p "_test.go" (buffer-file-name)))))
+;;   :next-checkers ((warning . go-errcheck)
+;;                   (warning . go-unconvert)
+;;                   (warning . go-staticcheck)))
+
+;; (with-eval-after-load 'flycheck
+;;    (add-to-list 'flycheck-checkers 'go-build-escape)
+;;    (flycheck-add-next-checker 'go-gofmt 'go-build-escape))
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
